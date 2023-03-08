95f9891f000c92c96ac2624d06bd3c20781f7ca9
ilyab <i-baykov@internet.ru>
2023-03-08 19:13:45 +0300

    В класс TattooStyle добавил метод проверки наличия стиля availability_check

--- coolsite/tattoo/supports/support_cls.py
+++ coolsite/tattoo/supports/support_cls.py
@@ -8,22 +8,22 @@ class MainData:
         self.__link_text = link_text
 
     @property
-    def title(self):
+    def title(self) -> str:
         return self.__title
 
     @property
-    def header(self):
+    def header(self) -> str:
         return self.__header
 
     @property
-    def description(self):
+    def description(self) -> str:
         """ Принимает ссылку на файл и возвращеает его текст """
         current_path = MainData.PATH + f"\{self.__link_text}"
         with open(current_path, "r", encoding="utf-8") as main_text:
             content = main_text.read()
             return content
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Экземпляр класса {self.__title} {self.__header} "
 
 
@@ -37,23 +37,25 @@ class TattooStyle:
         TattooStyle.__TATTOO_STYLE_DICT.setdefault(position, []).extend([style_en, style_ru])
 
     @property
-    def position(self):
+    def position(self) -> int:
         return self.__position
 
     @property
-    def style_en(self):
+    def style_en(self) -> str:
         return self.__style_en
 
     @property
-    def style_ru(self):
+    def style_ru(self) -> str:
         return self.__style_ru
 
     @classmethod
-    def tattoo_style_dict(cls):
+    def tattoo_style_dict(cls) -> dict:
         return cls.__TATTOO_STYLE_DICT
 
-    def __str__(self):
+    @classmethod
+    def availability_check(cls, style: str) -> bool:
+        """ Проверяет наличие style  в списке всех стилей """
+        return style.lower() in [stl[0].lower() for stl in TattooStyle.__TATTOO_STYLE_DICT.values()]
+
+    def __str__(self) -> str:
         return f"({self.__position}, {self.__style_en}, {self.__style_ru}"
-
-
-
