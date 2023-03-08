adae97f169e695533aac145702f51bb0184bc561
ilyab <i-baykov@internet.ru>
2023-03-08 16:19:42 +0300

    Реализовал вывод стартовой страницы с основной информацией

--- coolsite/tattoo/views.py
+++ coolsite/tattoo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .supports import support_function as sf
+
 
 # Create your views here.
+
+def main_menu(request):
+    contex = {
+        "title_info": sf.main_menu_info.get("title_info"),  # Возвращает текст для <title>
+        "header": sf.main_menu_info.get("header"),
+        "description": sf.main_menu_info.get("description")  # Возвращает описание страницы
+    }
+    return render(request, "tattoo_main_info.html", context=contex)
